c7efd959b8a04eff3372de191c180ac995fb46cc
ThiagoPeralti <68983937+ThiagoPeralti@users.noreply.github.com>
2022-05-27 17:39:36 -0300

    Commit_Aula_Laravel-20_05_2022

---
+++ storage/framework/views/23df3b9dd676fdb17288bcfdb782a4877f6c054b.php
@@ -0,0 +1,2 @@
+<h1>Esta é a página de contato.</h1>
+<a href="/">Voltar</a><?php /**PATH C:\Users\thiag\Desktop\IFSP\5° Semestre\Desenvolvimento Web II\hdcevents\resources\views/contact.blade.php ENDPATH**/ ?>
