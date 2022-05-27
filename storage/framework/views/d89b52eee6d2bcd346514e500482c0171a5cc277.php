10fd95a91b192e2d2ac022359fdb9cf6182ed082
ThiagoPeralti <68983937+ThiagoPeralti@users.noreply.github.com>
2022-05-27 17:30:57 -0300

    Video Aula Laravel  - 20/05/2022

---
+++ storage/framework/views/d89b52eee6d2bcd346514e500482c0171a5cc277.php
@@ -0,0 +1,8 @@
+
+
+<?php $__env->startSection('title', 'Produtos'); ?>
+
+<?php $__env->startSection('content'); ?>
+<h1>Tela de Produtos</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\thiag\Desktop\IFSP\5° Semestre\Desenvolvimento Web II\hdcevents\resources\views/products.blade.php ENDPATH**/ ?>
