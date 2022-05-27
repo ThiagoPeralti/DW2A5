acd972a6a3daaa76b60f4e0adc419a05a026bf32
ThiagoPeralti <68983937+ThiagoPeralti@users.noreply.github.com>
2022-05-27 17:35:44 -0300

    Delete storage directory

--- storage/framework/views/23df3b9dd676fdb17288bcfdb782a4877f6c054b.php
+++
@@ -1,2 +0,0 @@
-<h1>Esta é a página de contato.</h1>
-<a href="/">Voltar</a><?php /**PATH C:\Users\thiag\Desktop\IFSP\5° Semestre\Desenvolvimento Web II\hdcevents\resources\views/contact.blade.php ENDPATH**/ ?>
